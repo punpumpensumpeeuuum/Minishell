b31dbd1e6dd82f95bd5d4ced0a75b96829043ea6
Dinis Da Silva E Ferreira Da Conceicao <dinda-si@c1r5s2.42lisboa.com>
2024-06-06 17:33:21 +0100

    uma maca ao dia e bom

--- executecmd.c
+++ executecmd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   executecmd.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: elemesmo <elemesmo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dinda-si <dinda-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 17:08:32 by dinda-si          #+#    #+#             */
-/*   Updated: 2024/06/03 00:08:20 by elemesmo         ###   ########.fr       */
+/*   Updated: 2024/06/06 17:10:31 by dinda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ void	arrangepipes(t_vars *mini, int i)
 {
 	if (!mini->redrct && i == 0)
 	{
+		// ft_printf("bbbb: %d\n", i + 1);
+		// ft_printf("bb22: %d\n", mini->fd[i + 1]);
 		dup2(mini->fd[i + 1], 1);
-
+		return ;
 	}
 	if (!mini->redrct && i < numpipe(mini->input))
 	{
@@ -25,11 +27,14 @@ void	arrangepipes(t_vars *mini, int i)
 		// ft_printf("1: %d\n", mini->fd[2 * i + 1]);
 		dup2(mini->fd[2 * (i - 1)], 0);
 		dup2(mini->fd[2 * i + 1], 1);
+		return ;
 	}
 	else if (!mini->redrct && i == numpipe(mini->input))
 	{
-		// ft_printf("aaa0: %d\n", mini->fd[2 * (i - 1)]);
+		// ft_printf("aaa0: %d\n", 2 * (i - 1));
+		// ft_printf("aaa1: %d\n", mini->fd[2 * (i - 1)]);
 		dup2(mini->fd[2 * (i - 1)], 0);
+		return ;
 	}
 }
 
@@ -40,7 +45,7 @@ void	indicateredi(int flagfd, int *fd, char *redirection)
 		fd[0] = open(redirection, O_RDONLY);
 		if (fd[0] == -1)
 		{
-			ft_printf("%s: No such file or directory", redirection);
+			// ft_printf("%s: No such file or directory", redirection);
 			return ;
 		}
 		dup2(fd[0], 0);
@@ -61,12 +66,12 @@ void	execute(t_vars *mini, char **env, int i)
 	{
 		if (fastcheckpath(mini, env, 1, 0) == 0)
 		{
-			ft_printf("%s: command not found\n", mini->trueflag[0]);
+			// ft_printf("%s: command not found\n", mini->trueflag[0]);
 			return ;
 		}
 		while (i <= p)
 		{
-			ft_printf("1a\n");
+			// ft_printf("1a\n");
 			mini->flag = ft_split(mini->input, '|');
 			mini->trueflag = ft_goodsplit(mini->flag[i]);
 			mini->flagfd = 2;
@@ -86,14 +91,27 @@ void	veryexecute(t_vars *mini, char **env, int i)
 	mini->pid = fork();
 	if (mini->pid == 0)
 	{
+		int li = 0;
+		while (mini->fd[li])
+		{
+			ft_printf("fd: %d\n", mini->fd[li]);
+			ft_printf("i:  %d\n", li);
+			li++;
+		}
+		// ft_printf("IMPORTATNTE: %d\n", mini->pid);
 		redirect(mini, mini->input);
+		// ft_printf("mmmm\n");
 		arrangepipes(mini, i);
+		// ft_printf("mmnnnnnmm\n");
 		indicateredi(mini->flagfd, mini->fd, mini->redrct);
 		execve(mini->check, mini->trueflag, env);
 		exit(2);
 	}
 	else
+	{
+		waitpid(mini->pid, NULL, 0);
 		return ;
+	}
 }
 
 void	executeone(t_vars *mini, char **env)
